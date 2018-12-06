b92893d1815b09de4aafc0a856fb71153123d9a4
Jeonghwan Bhan <jhbhan@gmail.com>
2018-12-06 09:37:52 -0800

    hws

--- Haskell/hw3.hs
+++ Haskell/hw3.hs
@@ -2,18 +2,57 @@ import Data.List
 import System.IO
 
 --without pattern matching
+--myremoveduplicates :: Eq a => [a] -> [a]
 myremoveduplicates mylist | null mylist = mylist | elem (head mylist) (tail mylist) = myremoveduplicates (tail mylist) | otherwise = (head mylist):myremoveduplicates (tail mylist)
 
 --with pattern matching
+--myremoveduplicates_pm :: Eq a => [a] -> [a]
+myremoveduplicates_pm [] = []
+myremoveduplicates_pm (x:[]) = [x]
+myremoveduplicates_pm (x:y:[]) = if x == y then [x] else x:y:[]
+myremoveduplicates_pm (x:xs)| elem x xs = myremoveduplicates_pm xs | otherwise = x:(myremoveduplicates_pm xs)
+
 
 --without pattern matching
+--myintersection :: (Foldable t, Eq a) => [a] -> t a -> [a]
 myintersection mylist1 mylist2 | null mylist1 || null mylist2 = [] | elem (head mylist1) mylist2 = (head mylist1):(myintersection (tail mylist1) mylist2)|otherwise = myintersection (tail mylist1) mylist2
+--with pattern matching
+--myintersection_pm :: Eq a => [a] -> [a] -> [a]
+myintersection_pm x [] = []
+myintersection_pm [] y = []
+myintersection_pm (x:xs) y |elem x y = x:(myintersection_pm xs y) |otherwise = myintersection_pm xs y
+
+
 
 --without pattern matching
-mylast mylist | length mylist == 1 = mylist | otherwise = mylast (tail mylist)
+--mylast :: Eq a => [a] -> [a]
+mylast mylist | null mylist = mylist | (tail mylist) == [] = mylist | otherwise = mylast (tail mylist)
+--with pattern 
+--mylast_pm :: [a] -> [a]
+mylast_pm [] = []
+mylast_pm (x:[]) = [x]
+mylast_pm (x:y:[]) = [y]
+mylast_pm (x:xs) = mylast_pm xs
+
+--without patternmatching
+--myreverse :: Eq a => [a] -> [a]
+myreverse mylist = myreversehelp mylist [] where
+myreversehelp mylist reversed |null mylist = mylist| (tail mylist) == [] = (head mylist):reversed|otherwise = myreversehelp (tail mylist) ((head mylist):reversed)
+--with pattern matching
+--myreverse_pm :: Eq a => [a] -> [a]
+myreverse_pm mylist = myreversehelp mylist [] where
+myreversehelp_pm [] reversed = reversed
+myreversehelp_pm (x:xs) reversed = myreversehelp_pm xs (x:reversed)
+
+
 
 --without pattern matching
-myreverse mylist | null mylist  = []| otherwise = myreverse (tail mylist) ++ [(head mylist)]
-
---without pattern matching	
+--myreplaceall :: Eq a => a -> a -> [a] -> [a]
 myreplaceall x y mylist| null mylist = []| y == (head mylist) = x:myreplaceall x y (tail mylist) | otherwise = (head mylist):myreplaceall x y (tail mylist)
+
+--with pattern matching
+--myreplaceall_pm :: Eq a => a -> a -> [a] -> [a]
+myreplaceall_pm x y [] = []
+myreplaceall_pm x y (z:[]) = if z == y then [x] else [z]
+myreplaceall_pm x y (z:w:[]) = if z == y then x:(myreplaceall_pm x y [w]) else z:(myreplaceall_pm x y [w])
+myreplaceall_pm x y (z:zs) = if z == y then x:(myreplaceall_pm x y zs) else z:(myreplaceall_pm x y zs)
---
+++ Prolog/hw4.pl
@@ -0,0 +1,9 @@
+shuffle([L1],[L2],[L1,L2]).
+shuffle([H1|T1],[H2|T2],[H31,H32|T3]) :- shuffle([H1],[H2],[H31,H32]),shuffle(T1,T2,T3).
+double([L1],[L1,L1]).
+double([H1|T1],[H21,H22|T2]) :- double([H1],[H21,H22]),double(T1,T2).
+no_duplicates([],[]).
+no_duplicates([H1|T1], L1) :- member(H1, T1),no_duplicates(T1, L1).
+no_duplicates([H1|T1], [H2|T2]) :- \+member(H1, T2),no_duplicates(T1, T2).
+same_elements(H1,H1).
+same_elements([H1|T1],L1) :- member(H1,L1),same_elements(T1,L1).
